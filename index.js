18322fcd2c538840bef5bfc89ba52be0f4ec26e3
Marior9 <mariorodriguezan@gmail.com>
2019-05-21 15:13:28 -0400

    missing setters

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your Circle class here
+// Add your Circle class here\
+
+console.log("Connected")
+
+class Circle{
+    constructor(radius){
+        this.radius = radius
+    }
+    get diameter(){
+        return this.radius * 2
+    }   
+    get circumference(){
+        return Math.PI * this.diameter
+    }
+    get area(){
+        return Math.PI * Math.pow(this.radius, 2)
+    }
+   set diameter(diameter){
+       this.diameter = diameter
+   }
+   set circumference(circumference){
+       this.circumference = circumference
+   }
+   set area(area){
+       this.area = area
+   }
+
+}
